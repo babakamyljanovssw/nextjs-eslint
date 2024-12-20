2d1b95069aeb6715ed87ad90f96839fa129a081b
Baba Kamyljanov <babakamyljanov@ssw.com.au>
2024-12-20 15:59:24 +0800

    add test classname

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <h3 className="prose prose-h1:!my-0 prose-h1:!pt-4 prose-h3:!mt-0 prose-img:!my-0 flex-1 pt-4"></h3>
-
+        <h4 className="prose prose-h1:!my-0 prose-h1:!pt-4 prose-h3:!mt-0 prose-img:!my-0 flex-1 pt-4">
+          Test
+        </h4>
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
